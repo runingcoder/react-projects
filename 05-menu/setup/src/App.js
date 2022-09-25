e48e554e5840751eeb163b70dbb5a311b1725e4e
Achyut Kayastha <achyutkayastha5@gmail.com>
2022-09-25 22:28:00 +0545

    first menu done

--- 05-menu/setup/src/App.js
+++ 05-menu/setup/src/App.js
@@ -1,10 +1,45 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Menu from './Menu';
 import Categories from './Categories';
 import items from './data';
+import menu from './data';
+
 
 function App() {
-  return <h2>menu project setup</h2>;
+  // get unique categories as an array,
+  // then map through the array and return a button for each category
+  // then add onClick event to each button
+  const category1 = ['all', ...new Set(items.map((item) => item.category))]
+  const allCategories = ['all', ...new Set(items.map((item) => item.category))];
+
+  const  [ menuItems, setMenuItems ] = useState(items);
+  const [ categories, setCategories ] = useState(allCategories);
+
+  const filterItems = (category) =>{
+    if (category === 'all'){
+      setMenuItems(items);
+      return;
+    }
+    const newItems =  items.filter((item)=>
+    item.category === category)
+      setMenuItems(newItems);
+  }
+
+  // useEffect(()=>{
+  //   filterItems()
+  // },[])
+
+  return <main>
+    <section className="menu section">
+      <div className="title">
+        <h2>our menu</h2>
+        <div className="underline"></div>
+
+        </div>
+        <Categories filterItems = {filterItems} categories = {categories}  />
+        <Menu items={menuItems} />
+        </section>
+  </main>
 }
 
 export default App;
